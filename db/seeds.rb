5658f1747052c73434cca81c1ae4c73bdaa61666
vkill <vkill.net@gmail.com>
2011-09-12 04:48:47 +0800

    add factory businesses  to db/seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,17 +13,17 @@ p '> Initializing primary departments ...'
 %w(财务处 后勤基建处 计算机工程系 图书信息中心).each do |name|
   Factory :department, :name => name, :describtion => ''
 end
-p '%s departments were created.' % Department.count
+p '   %s departments were created.' % Department.count
 
 p '> Initializing primary users ...'
 %w(vkill jerry).each do |name|
   user = Factory :user, :name => name, :email => '%s@gmail.com' % name
   user.add_role! 'admin'
 end
-p '%s users were created.' % User.count
+p '   %s users were created.' % User.count
 
 #p '> Initializing primary services ...'
-#p '%s services were created.' % Service.count
+#p '    %s services were created.' % Service.count
 
 
 p '> Generating test data'
@@ -35,9 +35,14 @@ Department.find_each do |department|
     posts = _3_posts :user => user
     softwares = _3_softwares :user => user
     softwares.each do |software|
-      _1_attachment :attachmentable => software
+      _1_attachment :attachmentable => software if rand(2) == 1
+    end
+
+    businesses = _3_businesses :user => user, :service => services.sample
+    businesses.each do |business|
+      _1_feedback :issue => business if rand(2) == 1
     end
   end
 end
-p 'done.'
+p '   done.'
 
